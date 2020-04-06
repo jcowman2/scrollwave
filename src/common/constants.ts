b4a13628bdce24dc64d77b05e803ef7ab0728916
jcowman2 <joe.cowman@huskers.unl.edu>
2020-04-06 14:52:20 -0500

    Animate blocks of text in reader

---
+++ src/common/constants.ts
@@ -0,0 +1,1 @@
+export const TEXT_VIEWPORT_HEIGHT = 400;
--- src/pages/view/ViewPage.tsx
+++ src/pages/view/ViewPage.tsx
@@ -1,22 +1,48 @@
 import React from "react";
 import { NodeGroup } from "react-move";
 import FullPage from "../../common/FullPage";
-import { ReaderData } from "../../common/common.types";
+import { ReaderData, IBlock } from "../../common/common.types";
+import { TEXT_VIEWPORT_HEIGHT } from "../../common/constants";
 
 export interface IViewPageProps {
   readerData: ReaderData;
+}
+
+interface IBlockState {
+  offsetVertical: number;
 }
 
 const ViewPage: React.FC<IViewPageProps> = props => {
   return (
     <FullPage>
       <div className="TextViewport">
-        {/* <NodeGroup></NodeGroup> */}
-        <div className="TextContent">
-          {props.readerData.blocks.map(block => (
-            <p key={block.key}>{block.text}</p>
-          ))}
-        </div>
+        <NodeGroup
+          data={props.readerData.blocks}
+          keyAccessor={(block: IBlock) => block.key}
+          start={(_item: IBlock, _index: number): IBlockState => ({
+            offsetVertical: TEXT_VIEWPORT_HEIGHT + 20
+          })}
+          enter={(_item: IBlock, index: number) => ({
+            offsetVertical: [-100], // TODO - account for multiline blocks
+            timing: { duration: 10000, delay: 1000 * index }
+          })}
+        >
+          {(
+            nodes: Array<{ key: string; data: IBlock; state: IBlockState }>
+          ) => (
+            <div className="TextViewportContent">
+              {nodes.map(({ key, data, state }) => (
+                <p
+                  className="TextViewportBlock"
+                  key={key}
+                  style={{ top: state.offsetVertical }}
+                >
+                  {data.text}
+                </p>
+              ))}
+            </div>
+          )}
+        </NodeGroup>
       </div>
     </FullPage>
   );
