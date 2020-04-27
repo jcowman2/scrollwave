6b5acc56cb42e526e92920dd3e4073966f2de5f8
jcowman2 <joe.cowman@huskers.unl.edu>
2020-04-27 14:37:11 -0500

    Updates

---
+++ precache-manifest.20ac3e540f12ae960465bc85c1254399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccad8ca3677ef518495b832ba7d3d76",
+    "url": "/scrollwave/index.html"
+  },
+  {
+    "revision": "22d4853fa67e8eee75b9",
+    "url": "/scrollwave/static/css/main.9ded1ac7.chunk.css"
+  },
+  {
+    "revision": "e99194d00944bd29fb88",
+    "url": "/scrollwave/static/js/2.130dc939.chunk.js"
+  },
+  {
+    "revision": "38e917889775ebe84d7d31be1112a890",
+    "url": "/scrollwave/static/js/2.130dc939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d4853fa67e8eee75b9",
+    "url": "/scrollwave/static/js/main.57e66e3a.chunk.js"
+  },
+  {
+    "revision": "7dd6d4bc4a82960afcaf",
+    "url": "/scrollwave/static/js/runtime-main.abfb5158.js"
+  }
+]);
